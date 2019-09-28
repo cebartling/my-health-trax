989e7b881cf4c25aa51f93f90d6344b76feb0aa7
Christopher Bartling <chris.bartling@gmail.com>
2019-09-28 10:51:48 -0500

    Fixed configuration key.

--- my-health-trax-consumer/src/auth0Configuration.js
+++ my-health-trax-consumer/src/auth0Configuration.js
@@ -1,4 +1,4 @@
 export const auth0Configuration = {
     domain: process.env.REACT_APP_AUTH0_DOMAIN,
-    client_id: process.env.REACT_APP_AUTH0_CLIENT_ID
+    clientId: process.env.REACT_APP_AUTH0_CLIENT_ID
 };
