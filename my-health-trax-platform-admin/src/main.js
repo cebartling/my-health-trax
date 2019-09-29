677d2b93265100279b2221bda4c8da070342a1c1
Christopher Bartling <chris.bartling@gmail.com>
2019-09-29 11:57:15 -0500

    Integrated Auth0 into the Vue app.

--- my-health-trax-platform-admin/src/main.js
+++ my-health-trax-platform-admin/src/main.js
@@ -2,8 +2,25 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Auth0Plugin } from "./auth";
 
 Vue.config.productionTip = false;
+
+const domain = process.env.VUE_APP_AUTH0_DOMAIN;
+const clientId = process.env.VUE_APP_AUTH0_CLIENT_ID;
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
 
 new Vue({
   router,
