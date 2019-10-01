4dc0c4c97afbb944fb01443cfcd3d32037eb4b30
Christopher Bartling <chris.bartling@gmail.com>
2019-09-30 20:16:26 -0500

    Adding columns to the user_accounts table.

--- account-services/src/main/db/main.groovy
+++ account-services/src/main/db/main.groovy
@@ -1,11 +1,26 @@
 databaseChangeLog() {
     changeSet(id: '201909100154', author: 'cebartling') {
         createTable(tableName: 'user_accounts') {
-            column(name: 'id', type: 'bigint', autoIncrement: true) {
+            column(name: 'id', type: 'uuid', defaultValue: 'uuid_generate_v4()') {
                 constraints(nullable: false, primaryKey: true)
+            }
+            column(name: 'given_name', type: 'nvarchar(40)') {
+                constraints(nullable: false)
+            }
+            column(name: 'family_name', type: 'nvarchar(50)') {
+                constraints(nullable: false)
+            }
+            column(name: 'locale', type: 'nvarchar(6)') {
+                constraints(nullable: false)
+            }
+            column(name: 'email', type: 'nvarchar(300)') {
+                constraints(nullable: false)
             }
             column(name: 'created_at', type: 'timestamp', defaultValueDate: 'now()')
             column(name: 'updated_at', type: 'timestamp', defaultValueDate: 'now()')
+        }
+        rollback() {
+            dropTable(tableName: 'user_accounts')
         }
     }
 }
