1693ef3631c69072352e0ed25fb8d89fe9e8d879
Christopher Bartling <chris.bartling@gmail.com>
2019-10-15 18:57:18 -0500

    Removed script section for Home component. Don't need it quite yet.

--- my-health-trax-platform-admin/src/views/Home.vue
+++ my-health-trax-platform-admin/src/views/Home.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="home">
-    <!-- Check that the SDK client is not currently loading before accessing is methods -->
     <img alt="Vue logo" src="../assets/logo.png" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-
-export default {
-  name: "home",
-  components: {}
-};
-</script>
