183b1568b47495a4424d0eee1f6763662bca3c7b
Christopher Bartling <chris.bartling@gmail.com>
2019-10-15 18:41:49 -0500

    Updated Auth0 SPA library.

--- my-health-trax-platform-admin/src/App.vue
+++ my-health-trax-platform-admin/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link v-if="$auth.isAuthenticated" to="/userProfile"
-        >User profile</router-link
-      >
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about">About</router-link>
+      <span v-if="$auth.isAuthenticated">
+        |
+        <router-link to="/userProfile">
+          User profile
+        </router-link>
+      </span>
     </div>
     <router-view />
   </div>
@@ -19,11 +23,14 @@
   text-align: center;
   color: #2c3e50;
 }
+
 #nav {
   padding: 30px;
+
   a {
     font-weight: bold;
     color: #2c3e50;
+
     &.router-link-exact-active {
       color: #42b983;
     }
