e152a8c2582bcc7728688a315d0c1e40254ffaee
Christopher Bartling <chris.bartling@gmail.com>
2019-11-11 21:40:22 -0600

    Getting things set up in Health Services.

--- health-services/src/main/java/org/myhealthtrax/healthservices/MainApplication.java
+++ health-services/src/main/java/org/myhealthtrax/healthservices/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HealthServicesApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HealthServicesApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
