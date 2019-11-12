3b29f28bcd2d00d45bb24fb8249e401e3377be43
Christopher Bartling <chris.bartling@gmail.com>
2019-11-11 19:39:38 -0600

    Created new health-services GraphQL project.

---
+++ health-services/src/main/java/org/myhealthtrax/healthservices/HealthServicesApplication.java
@@ -0,0 +1,13 @@
+package org.myhealthtrax.healthservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthServicesApplication.class, args);
+	}
+
+}
---
+++ health-services/src/test/java/org/myhealthtrax/healthservices/HealthServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myhealthtrax.healthservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
